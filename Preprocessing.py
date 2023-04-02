099607368afa39e00d7f42b92f27f909ea87b3d9
Karnan <krishkarnan1@gmail.com>
2023-04-02 14:28:13 +0800

    modified preprocessing

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,10 +1,19 @@
 import string
-
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.text import Tokenizer
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib as plt
 import re
+import nltk
+nltk.download()
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize, word_tokenize
+from wordcloud import WordCloud, STOPWORDS
 from abbreviations import abbreviations
 
 
@@ -60,5 +69,61 @@ def remove_emoji(text):
                                u"\U000024C2-\U0001F251"
                                "]+", flags=re.UNICODE)
     return emoji_pattern.sub(r'EMOJI',text)
+def transcription_sad(text):
+    eyes =  "[8:=;]"
+    nose = "['\-]"
+    smiley = re.compile(r'[8:=;][\'\-]?[(\\/]')
+    return smiley.sub(r'SADFACE',text)
+
+def transcription_smile(text):
+    eyes = "[8:=;]"
+    nose = "['\-]"
+    smiley = re.compile(r'[8:=;][\'\-]?[)dDp]')
+    return smiley.sub(r'SMILE',text)
+
+def transcription_heart(text):
+    heart=re.compile(r'<3')
+    return heart.sub(r'HEART',text)
 
 
+# For clean the text
+
+def clean_tweet(text):
+    text = remove_URL(text)
+    text= remove_HTML(text)
+    text=remove_not_ASCII(text)
+    text=remove_mention(text)
+    text=remove_number(text)
+    text=replace_abbrev(text)
+    text=remove_emoji(text)
+    text=transcription_smile(text)
+    text=transcription_sad(text)
+    text=transcription_heart(text)
+    return text
+
+
+
+dft["clean_text"]=dft["clean_text"].apply(clean_tweet)
+#ak
+
+#print(stopwords.words('english'))
+
+def toremove_stopword(text):
+    remove_stopword=[word for word in text.split() if word.lower() not in stopwords.words('english')]
+    return remove_stopword
+
+dft["clean_text"]=dft["clean_text"].apply(toremove_stopword)
+
+#print(dft)
+
+#Tokenization
+
+max_features=3000
+tokenizer=Tokenizer(num_words=max_features,split=' ')
+tokenizer.fit_on_texts(dft['clean_text'].values)
+X=tokenizer.texts_to_sequences(dft['clean_text'].values)
+X=pad_sequences(X)
+
+#print(X[0])
+
+#tokenizer.sequences_to_texts([[ 713,  154,   56, 1434,   14]])
